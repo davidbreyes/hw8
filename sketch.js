27dd22a0e49c09bab8c6ae05fece87a18ce246a8
davidbreyes <43257766+davidbreyes@users.noreply.github.com>
2018-11-03 04:29:28 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,6 +44,12 @@ let snd11;
 let snd12;
 let snd13;
 
+var phoneclosedog;
+var phoneclosed;
+var pcanimation;
+
+var eyesblink;
+
 function preload() {
   // soundFormats('mp3');
   // for (var i = 0; i < 10; i++) {
@@ -70,8 +76,8 @@ function preload() {
   //first 10 keys are numbers, starts at 0
   img = loadImage('hbptec18.png');
   img2 = loadImage('hbptec8.png');
-  img3 = loadImage('hbptec.png');
-  img4 = loadImage('hbptec.png');
+  img3 = loadImage('hbptec9.png');
+  img4 = loadImage('hbptec10.png');
   img5 = loadImage('hbptec11.png');
   img6 = loadImage('hbptec12.png');
   img7 = loadImage('hbptec13.png');
@@ -100,23 +106,35 @@ function preload() {
   img26 = loadImage('hbptec2.png');
   img27 = loadImage('hbptec3.png');
   img28 = loadImage('hbptec4.png');
+
+  //phoneclosed = loadImage('hbptec29.png', 'hbptec30.png', 0, 0, 0);
+  //pcanimation = loadImage(phoneclosed);
+
+  //eyesblink.addAnimation('hbptec.png', 'hbptec2.png', 'hbptec3.png', 'hbptec4.png');
 }
 
 function setup() {
-  createCanvas(400, 400);
-  textSize(100);
+  createCanvas(1280, 1920);
+  //textSize(100);
 }
 
 function draw() {
   background(1280, 1920);
   fill(255);
   //closed phone first
-  image(img23, 0,0,);
+  //imageMode(CENTER);
+  image(img23, 0, 0);
+    if (mouseIsPressed) {
+    image(img).push();
+    }
+
+  //clear();
+  //animation(pcanimation, 0, 0);
 }
 
 function keyPressed() {
   if (key == '0') {
-    sound(snd).play();
+    snd.play();
     //text('0', random(width), random(height));
     image(img, 0, 0);
 
@@ -124,103 +142,103 @@ function keyPressed() {
   if (key == '1') {
     //sounds[1].play();
     //text('1', random(width), random(height));
-    sound(snd1).play();
+    snd1.play();
     image(img2,0,0);
   }
   if (key == '2') {
     //sounds[2].play();
     //text('2', random(width), random(height));
-    sound(snd2).play();
+    snd2.play();
     image(img3, 0,0);
   }
   if (key == '3') {
     //sounds[3].play();
     //text('3', random(width), random(height));
-    sound(snd3).play();
+    snd3.play();
     image(img4, 0,0);
   }
   if (key == '4') {
     //sounds[4].play();
     //text('4', random(width), random(height));
-    sound(snd4).play();
+    snd4.play();
     image(img5, 0,0);
   }
   if (key == '5') {
     //sounds[5].play();
     //text('5', random(width), random(height));
-    sound(snd5).play();
+    snd5.play();
     image(img6, 0,0);
   }
   if (key == '6') {
     //sounds[6].play();
     //text('6', random(width), random(height));
-    sounds(snd6).play();
+    snd6.play();
     image(img7, 0,0);
   }
   if (key == '7') {
     //sounds[7].play();
     //text('7', random(width), random(height));
-    sound(snd7).play();
+    snd7.play();
     image(img8, 0,0);
   }
   if (key == '8') {
     //sounds[8].play();
     //text('8', random(width), random(height));
-    sound(snd8).play();
+    snd8.play();
     image(img9, 0,0);
   }
   if (key == '9') {
     //sounds[9].play();
     //text('9', random(width), random(height));
-    sound(snd9).play();
+    snd9.play();
     image(img10, 0,0);
   }
-  if (key == 'W') {
-    sound(snd12).play();
+  if (key == 'w') {
+    snd12.play();
     image(img15,0,0);
   }
-  if (key == 'A') {
-    sound(snd13).play();
+  if (key == 'a') {
+    snd13.play();
     image(img14,0,0);
   }
-  if (key == 'S') {
-    sound(snd12).play();
+  if (key == 's') {
+    snd12.play();
     image(img11,0,0);
   }
-  if (key == 'E') {
-    sound(snd13).play();
+  if (key == 'e') {
+    snd13.play();
     image(img13,0,0);
   }
-  if (key == 'D') {
-    sound(snd12).play();
+  if (key == 'd') {
+    snd12.play();
     image(img12, 0,0);
   }
   if (key == '-') {
-    sound(snd13).play();
+    snd13.play();
     image(img19,0,0);
   }
   if (key == '=') {
-    sound(snd12).play();
+    snd12.play();
     image(img20,0,0);
   }
-  if (key == 'Z') {
-    sound(snd13).play();
+  if (key == 'z') {
+    snd13.play();
     image(img16,0,0);
   }
-  if (key == 'V') {
-    sound(snd12).play();
+  if (key == 'v') {
+    snd12.play();
     image(img17,0,0);
   }
-  if (key == 'M') {
-    sounds(snd13).play();
+  if (key == 'm') {
+    snd13.play();
     image(img18,0,0);
   }
   if (key == '[') {
-    sound(snd12).play();
+    snd12.play();
     image(img21,0,0)
   }
   if (key == ']') {
-    sounds(snd13).play();
+    snd13.play();
     image(img22,0,0);
   }
 //
